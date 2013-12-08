1e1c296e638ae0e40258de6039982e5a04ed4ac1
Chris911 <Christophe.naud.dulude@gmail.com>
2013-12-08 02:00:11 -0500

    Addind get_horaire method

--- lib/PolyNotify/dossier_etudiant.rb
+++ lib/PolyNotify/dossier_etudiant.rb
@@ -15,6 +15,14 @@ module PolyNotify
       extract_notes doc
     end
 
+    def get_horaire(session)
+      @inputs['trimestre'] = session
+      body = @inputs
+      data = post('/servlet/PresentationHorairePersServlet', :body => body, :headers => {'Content-Type' => 'application/x-www-form-urlencoded'})
+      doc = Nokogiri.parse(data)
+      extract_horaire doc
+    end
+
     def save_data (doc)
       doc.css('input').each do |input|
         @inputs[input['name']] = input['value'] unless input['name'].include? 'btn'
@@ -26,5 +34,9 @@ module PolyNotify
       @checksum = Digest::MD5.hexdigest(tables[0])
     end
 
+    def extract_horaire(doc)
+      horaire = doc.css('table').select {|table| table['cellspacing'] == '5'}
+      puts horaire
+    end
   end
 end

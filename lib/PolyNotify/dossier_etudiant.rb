826ef6c505beca701668ed79054f2b9edc725f02
Chris911 <Christophe.naud.dulude@gmail.com>
2013-12-08 01:43:39 -0500

    Reformat methods

--- lib/PolyNotify/dossier_etudiant.rb
+++ lib/PolyNotify/dossier_etudiant.rb
@@ -4,24 +4,24 @@ module PolyNotify
     def log_in
       body = {code: @config['CODE'], nip: @config['PASSWORD'], naissance: @config['DDN']}
       data = post('/servlet/ValidationServlet', :body => body, :headers => {'Content-Type' => 'application/x-www-form-urlencoded'})
-      save_data data
+      doc = Nokogiri.parse(data)
+      save_data doc
     end
 
     def get_resultats_finaux
       body = @inputs
       data = post('/servlet/PresentationResultatsTrimServlet', :body => body, :headers => {'Content-Type' => 'application/x-www-form-urlencoded'})
-      extract_notes data
+      doc = Nokogiri.parse(data)
+      extract_notes doc
     end
 
-    def save_data (data)
-      doc = Nokogiri.parse(data)
+    def save_data (doc)
       doc.css('input').each do |input|
         @inputs[input['name']] = input['value'] unless input['name'].include? 'btn'
       end
     end
 
-    def extract_notes(data)
-      doc = Nokogiri.parse(data)
+    def extract_notes(doc)
       tables = doc.css('table').select {|table| table['width'] == '75%'}
       @checksum = Digest::MD5.hexdigest(tables[0])
     end
